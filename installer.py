5873d978e5d794c334819c1c19ead5b95a8e8b6f
klundert <r.van.de.klundert@vu.nl>
2022-09-30 15:19:00 +0200

    change conda to mamba install

--- installer.py
+++ installer.py
@@ -5,8 +5,8 @@ assert "Python 3." in os.popen("python -V").read(), "Get python 3 you fool"
 
 os.popen("conda config --add channels conda-forge").read()
 
-print("Installing dependencies with conda...")
-os.popen(f"conda install --yes numpy scipy nilearn joblib statsmodels").read()
+print("Installing dependencies with mamba...")
+os.popen(f"mamba install --yes numpy scipy nilearn joblib statsmodels").read()
 
 print("Installing prfpy and verifying dependencies with pip...")
 os.popen("python -m pip install -e .").read()
