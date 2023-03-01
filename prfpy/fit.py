CF gridfit params were stored in the wrong order for follow-up iterative fitting. Fixed this order

prfpy/fit.py
@@ -1383,15 +1383,15 @@ class CFFitter(Fitter):
 
         # output
         self.gridsearch_params = np.array([
-            self.model.vert_centres_flat[max_rsqs],
             self.model.sigmas_flat[max_rsqs],
             self.best_fitting_beta,
-            self.best_fitting_baseline,
+            self.best_fitting_baseline,          
+            self.model.vert_centres_flat[max_rsqs],
             self.gridsearch_r2
         ]).T
         
         # Put the vertex centres into a dictionary.
-        self.vertex_centres=self.gridsearch_params[:,0].astype(int)
+        self.vertex_centres=self.gridsearch_params[:,3].astype(int)
         self.vertex_centres_dict = [{'vert':k} for k in self.vertex_centres]
         
     def quick_grid_fit(self,sigma_grid):

prfpy/model.py
@@ -789,7 +789,11 @@ class CFGaussianModel():
             
         # We can grab the row of the distance matrix corresponding to this vertex and make the rf.
         rf=np.array([gauss1D_cart(self.stimulus.distance_matrix[idx], 0, sigma)])
-            
+        
+        # normalize beta and rf by rf
+        beta = beta/np.sum(rf)
+        rf=rf/np.sum(rf)
+        
         # Dot with the data to make the predictions. 
         neural_tc = stimulus_through_prf(rf, self.stimulus.design_matrix, 1)
     
@@ -894,8 +898,9 @@ class Norm_CFGaussianModel(CFGaussianModel):
         
             
         # normalize amplitudes and cfs by cfs
-        sCF_amplitude = srf_amplitude/np.sum(sCF)
         CF_amplitude = prf_amplitude/np.sum(CF)
+        sCF_amplitude = srf_amplitude/np.sum(sCF)
+        
         
         CF=prf/np.sum(CF)
         sCF=srf/np.sum(sCF)